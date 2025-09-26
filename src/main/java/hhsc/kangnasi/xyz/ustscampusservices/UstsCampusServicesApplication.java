ed93acd2bad579d0f2c01769bee9bbf56f46eae1
zq <2419646091@qq.com>
2025-09-26 21:32:50 +0800

    init

---
+++ src/main/java/hhsc/kangnasi/xyz/ustscampusservices/UstsCampusServicesApplication.java
@@ -0,0 +1,13 @@
+package hhsc.kangnasi.xyz.ustscampusservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstsCampusServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UstsCampusServicesApplication.class, args);
+    }
+
+}
