d13d243567040d2cf763f1ede47601906acad503
zq <2419646091@qq.com>
2025-09-27 14:48:33 +0800

    登录拦截

---
+++ src/main/java/hhsc/kangnasi/xyz/ustscampusservices/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package hhsc.kangnasi.xyz.ustscampusservices.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    public WebMvcConfig(AuthInterceptor authInterceptor) {
+        this.authInterceptor = authInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        // auth endpoints
+                        "/user/send-login-code",
+                        "/user/login",
+                        "/user/validate-token",
+                        // health/error/static
+                        "/error",
+                        "/favicon.ico"
+                );
+    }
+}
+
